20cf344626543374e47e03ab78848cb89036d033
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2023-01-16 19:12:28 -0500

    Add files via upload

--- VideoFrameTextEditor/main.js
+++ VideoFrameTextEditor/main.js
@@ -6,19 +6,26 @@ var coverSection = document.getElementById("cover-section");
 var outputSection = document.getElementById("output-section");
 var plainBackgroundColorPicker = document.getElementById("plain-background-color-picker");
 var plainColorBackground = document.getElementById("plain-color-background");
+var tabletScreen = document.getElementById("tablet-screen");
+var cover = document.getElementById("cover");
+var excerpt = document.getElementById("excerpt");
 
-function toggleInput(){
+function toggleCoverExcerpt(){
     if (isParagraphMode) {
-        inputHeading.innerText = 'Add your cover';
+        inputHeading.innerText = 'Choose your cover image';
         inputToggleButton.innerText = 'display text instead of book cover';
         coverSection.classList.remove('hidden');
         paragraphInputSection.classList.add('hidden');
+        cover.classList.remove('hidden');
+        excerpt.classList.add('hidden');
         isParagraphMode = false;
     } else {
         inputHeading.innerText = 'Enter and format your text';
         inputToggleButton.innerText = 'display book cover instead of text';
         paragraphInputSection.classList.remove('hidden');
         coverSection.classList.add('hidden');
+        excerpt.classList.remove('hidden');
+        cover.classList.add('hidden');
         isParagraphMode = true;
     }
 }
@@ -32,6 +39,7 @@ function setBackground(e) {
     let oldBackgroundClass = outputSection.classList[1];
     if (backgroundClass != oldBackgroundClass) {
         outputSection.style.backgroundColor.remove;
+        outputSection.style.backgroundImage.remove;
         outputSection.classList.add(backgroundClass);
         outputSection.classList.remove(oldBackgroundClass);
     }
@@ -55,4 +63,17 @@ function setPlainColorBackground() {
 
 function closePlainColorBackgroundPicker() {
     plainBackgroundColorPicker.classList.add("hidden");
+}
+
+function fitCover() {
+    console.log('fitting cover');
+    console.log(cover.classList);
+    cover.classList.remove('fill-cover');
+    cover.classList.add('fit-cover');
+    console.log(cover.classList);
+}
+
+function fillCover() {
+    cover.classList.remove('fit-cover');
+    cover.classList.add('fill-cover');
 }
